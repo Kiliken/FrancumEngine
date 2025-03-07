5209f442542550502a821fb314872bf5d21f5555
Kiliken <francescopaolomariani7@gmail.com>
2025-03-07 17:28:10 +0900

    First Setup

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+SET includes=/Isrc /I%cd%/Dependencies/Include
+SET links=/link /LIBPATH:%cd%/Dependencies/Lib/glew-2.1.0 /LIBPATH:%cd%/Dependencies/Lib/glfw-vc2022 MSVCRT.lib glew32s.lib glfw3.lib opengl32.lib User32.lib Shell32.lib Gdi32.lib Kernel32.lib Advapi32.lib
+SET defines=/D DEBUG
+
+cl /EHsc %includes% %defines% src/main.cpp %links%
